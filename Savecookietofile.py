ea51145086391214d41e7379d8094c0fcab97655
yangqihang <442621753@qq.com>
2016-08-04 18:35:21 +0800

    save cookie to file

---
+++ Savecookietofile.py
@@ -0,0 +1,9 @@
+import cookielib
+import urllib2
+
+filename='cookie.txt'
+cookie=cookielib.MozillaCookieJar(filename)
+handler=urllib2.HTTPCookieProcessor(cookie)
+opener=urllib2.build_opener(handler)
+response=opener.open("http://www.baidu.com")
+cookie.save(ignore_discard=True,ignore_expires=True)
